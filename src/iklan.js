aab6203dda6d65697cabff0573e76c072a162298
seu nome <you@example.com>
2021-05-29 14:42:40 -0300

    First commit

---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 OSVALDINHO 2.0 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/555491057713*
+║
+╚═〘  OSVALDINHO 2.0 〙
+`
+}
+exports.iklan = iklan
